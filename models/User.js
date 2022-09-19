fd3d9950de25f485e741a67e5c8d85750123a28e
tonyclem <tonyclem001@gmail.com>
2022-09-19 12:32:47 +0200

    added user schema

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "The name is required"],
+    minlength: 3,
+    maxlength: 25,
+  },
+  email: {
+    type: String,
+    required: [true, "The email is required"],
+    validated: {
+      validator: validator.isEmail,
+      message: "Please provide valid email",
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "The password is required"],
+    minlength: 8,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
